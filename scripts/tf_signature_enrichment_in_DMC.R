c33c19b7bd218f3d0750e59ae048204fb6f49975
Alexandre Pelletier <apelletier@egid.local>
2021-03-25 17:38:40 +0100

    figures tf signature enrichment in DMC by lin

--- scripts/tf_signature_enrichment_in_DMC.R
+++ scripts/tf_signature_enrichment_in_DMC.R
@@ -34,20 +34,76 @@ fwrite(res_gsea,fp(out,"res_cflf_gsea_genescore_ranked.csv.gz"),sep=";")
 #for each compa
 #run tf1-
 
-res_gsea_all<-fread("outputs/tf_signature_enrichment_in_DMC/res_gsea_genescore_ranked.csv.gz")
+res_gsea_all<-fread("../methyl/outputs/tf_signature_enrichment_in_DMC/res_gsea_genescore_ranked.csv.gz")
 
 res_gsea_all[,top10:=padj<=sort(padj)[10],by="compa"]
 
 res_gsea_all[top10==T,.(pathway,compa,padj,NES)]
 res_gsea_all[pathway=="STAT3",.(pathway,compa,padj,NES)]
 
-#=> TF epigen alteration by lineage[a finir]
+#C vs L
+#top 20 regulons by gsea in C vs L
+res_gsea<-res_gsea_all[compa=="C.L"][,regulon:=pathway][,-"pathway"]
+regulons_ord<-res_gsea[padj<=sort(padj)[15]][order(padj)]$regulon
 
-regulons_lineage<-
+ggplot(res_gsea[padj<=sort(padj)[15]])+geom_col(aes(x=regulon,y=-log10(padj),fill=NES))+scale_x_discrete(limits=regulons_ord)
+
+regulons_ord<-res_gsea[!str_detect(regulon,'e$')][padj<=sort(padj)[15]][order(padj)]$regulon
+ggplot(res_gsea[!str_detect(regulon,'e$')][padj<=sort(padj)[15]])+
+  geom_col(aes(x=regulon,y=-log10(padj),fill=NES))+
+  scale_x_discrete(limits=regulons_ord)
+
+#=> TF epigen alteration by lineage
+res_gsea<-res_gsea_all[compa=="C.L"][,regulon:=pathway][,-"pathway"]
+
+regulons_lineage<-fread("../singlecell/outputs/06-SCENIC/cbps0-8_clean/tf_markers_lineage.csv.gz")
+
+regulons_lineage_f<-regulons_lineage[p_val_adj<10^-30&avg_log2FC>0]
+table(regulons_lineage_f$lineage)
+res_gsea_lineage<-merge(res_gsea,regulons_lineage_f[,.(regulon,lineage,p_val_adj)])
+
+table(res_gsea_lineage[padj<0.001]$lineage)
+res_gsea_lineage[padj<0.001&!str_detect(regulon,'e$')]
+
+ggplot(res_gsea_lineage[padj<0.001&!str_detect(regulon,'e$')])+geom_bar(aes(x=lineage))
+ggplot(res_gsea_lineage[padj<0.001&!str_detect(regulon,'e$')])+geom_boxplot(aes(x=lineage,y=-log10(padj)))
+
+
+
+#CF vs LF
+#top 20 regulons by gsea in C vs L
+res_gsea<-res_gsea_all[compa=="CF.LF"][,regulon:=pathway][,-"pathway"]
+regulons_ord<-res_gsea[padj<=sort(padj)[15]][order(padj)]$regulon
+
+ggplot(res_gsea[padj<=sort(padj)[15]])+geom_col(aes(x=regulon,y=-log10(padj),fill=NES))+scale_x_discrete(limits=regulons_ord)
+
+regulons_ord<-res_gsea[!str_detect(regulon,'e$')][padj<=sort(padj)[15]][order(padj)]$regulon
+ggplot(res_gsea[!str_detect(regulon,'e$')][padj<=sort(padj)[15]])+
+  geom_col(aes(x=regulon,y=-log10(padj),fill=NES))+
+  scale_x_discrete(limits=regulons_ord)
+
+#=> TF epigen alteration by lineage
+res_gsea<-res_gsea_all[compa=="CF.LF"][,regulon:=pathway][,-"pathway"]
+
+res_gsea_lineage<-merge(res_gsea,regulons_lineage_f[,.(regulon,lineage,p_val_adj)])
+res_gsea_lineage<-res_gsea_lineage[padj<0.001&p_val_adj<0.001]
+ggplot(res_gsea_lineage[!str_detect(regulon,'e$')&lineage%in%c("HSC","MPP","Erythroid","Lymphoid","Myeloid")])+
+  geom_col(aes(x=regulon,y=-log10(padj),fill=-log10(p_val_adj)))+facet_grid( .~ lineage, space = "free_x",scales="free_x")+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1,size = 9),
+        axis.title=element_text(size=12,face="bold"))
+
+table(res_gsea_lineage[padj<0.001]$lineage)
+res_gsea_lineage[padj<0.001&!str_detect(regulon,'e$')]
+
+ggplot(res_gsea_lineage[padj<0.001&!str_detect(regulon,'e$')])+geom_bar(aes(x=lineage))
+ggplot(res_gsea_lineage[padj<0.001&!str_detect(regulon,'e$')])+geom_boxplot(aes(x=lineage,y=-log10(padj)))
+
+
 
 
 #over representation test [à finir]
 res_meth[,gene_score_scaled:=scale(GeneScore,center = F),by="compa"]
 unique(res_meth[gene=="SOCS3",.(gene,compa,GeneScore,gene_score_scaled)])
 unique(res_meth[gene=="HES1",.(gene,compa,GeneScore,gene_score_scaled)])
+
 
