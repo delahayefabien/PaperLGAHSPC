30f536243e396684ea908760cb4cb4a1b49aa4ff
Alexandre Pelletier <apelletier@egid.local>
2021-04-08 12:05:40 +0200

    correl stress meth

--- scripts/figures_paper_methyl.R
+++ scripts/figures_paper_methyl.R
@@ -1,9 +1,12 @@
 #figures paper methyl
+#see other test on figures_paper_methyl_annexe
 source("../methyl/scripts/utils/new_utils.R")
 library(limma)
 library(pheatmap)
 out<-"../methyl/outputs/figures_paper_meth"
 dir.create(out)
+
+
 #validation cohorts
 meth<-fread("../methyl/datasets/cd34/2020-05-25_methyl_data_before_limma.csv")
 mtd<-fread("../methyl/datasets/cd34/cleaned_batch_CD34_library_date_220620.csv")
@@ -63,13 +66,11 @@ fit2  <- contrasts.fit(fit, cont.matrix)
 fit2  <- eBayes(fit2)
 
 #merge res cohort 1 and 2
-
 res<-rbind(res[,cohort:=2],
            Reduce(rbind,lapply(colnames(cont.matrix),
                                function(comp)data.table(topTable(fit2,coef = comp,n = Inf),
                                                         keep.rownames = "cpg_id")[,compa:=comp]))[,cohort:=1])
 fwrite(res,fp(out,"res_limma_cohorts.tsv.gz"),sep="\t")
-
 #cl
 ggplot(res[compa%in%c("C.L")])+
   geom_point(aes(x=logFC,y=-log10(P.Value),col=P.Value<10^-4&abs(logFC)>30),size=)+
@@ -88,58 +89,48 @@ ggplot(res[compa%in%c("CM.LM")])+
   facet_wrap("cohort")+
   scale_color_manual(values = c("grey","red"))
 
-
 table(res[P.Value<10^-4&abs(logFC)>30][,hyper_meth:=logFC>0][,.(hyper_meth,compa,cohort)])
 
-#genescore heatmap
-meth_df<-melt(meth,id.vars = 1,
-              variable.name = "sample",
-              value.name = "methylation")
-cpgs_weight<-fread("../methyl/ref/2020-06-29_All_CpG-Gene_links.csv")
-cpgs_weight[,cpg_weight:=RegWeight+LinksWeight]
-meth_df<-merge(meth_df,cpgs_weight[,.(locisID,gene,cpg_weight)],by="locisID")
-meth_df[,gene_meth:=sum(methylation*cpg_weight)/sum(cpg_weight),by=c("sample","gene")]
-fwrite(meth_df,fp(out,"methylation_sum_by_gene_datatable.tsv.gz"),sep="\t")
-meth_genes<-unique(meth_df,by=c("sample","gene"))
-
-meth_genes[,gene_meth_scaled:=scale(gene_meth),by=c("sample")]
-
-meth_genes_mat<-dcast(meth_genes[,-c("locisID","hi.conf.genes","methylation","gene_meth")],sample~gene)
-meth_genes_mat[1:10,1:10]
-
-meth_genes_mat<-as.matrix(data.frame(meth_genes_mat,row.names = "sample"))
-meth_genes_mat<-t(meth_genes_mat)
-meth_genes_mat[1:10,1:10]
-dim(meth_genes_mat)
 
 
-pheatmap(meth_genes_mat,
-         annotation_col = data.frame(mtd,row.names = "sample")[colnames(meth_genes_mat),c("Group_Sex","Group_Complexity_Fac")],
-         show_rownames = F,show_colnames = F
-        ) #doesn work
-#see other test on figures_paper_methyl_annexe
 
-#see key genes
+#res meth CL
+res_cl<-fread("../methyl/outputs/model14_without_iugr/2020-09-16_res_C.L_with_GeneScore_and_permut.csv")
 
-ggplot(meth_genes[gene%in%c("SOCS3","HES1","JUN")&Group_name%in%c("C","L")])+
-geom_boxplot(aes(x=Group_Sex,y=gene_meth))+facet_wrap("gene")
+ggplot(res_cl)+
+  geom_point(aes(x=meth.change,y=-log10(pval),col=pval<10^-3&abs(meth.change)>30),size=0.1)+
+  scale_color_manual(values = c("grey","red"))
 
-#res
+
+res_cl[pval<10^-3&meth.change>30]
+res_cl[,padj:=p.adjust(pval,method = 'BH')]
+res_cl[padj<0.05]
+resg_cl<-unique(res_cl[order(gene,pval)],by="gene")
+resg_cl[GeneScore>60&pval<10^-3]
+
+plot(density(resg_cl$GeneScore))
+abline(v=60)
+ggplot(resg_cl)+
+  geom_point(aes(x=GeneScore,y=-log10(pval),col=pval<10^-3&abs(GeneScore)>60),size=0.1)+
+  scale_color_manual(values = c("grey","red"))
+
 res<-fread("../methyl/outputs/model14_without_iugr/2020-10-08_all_res_with_perm.csv")
-resg<-unique(res[order(gene,pval)],by="gene")
-ggplot(resg)+geom_density(aes(x=GeneScore))+facet_wrap("compa")
 
-resg[,gs_scaled:=scale(GeneScore,center=F),by="compa"]
-ggplot(resg)+geom_density(aes(x=gs_scaled))+facet_wrap("compa")
 
-p1<-ggplot(resg[compa%in%c("C.L","CF.LF","CM.LM")],aes(x=compa,y=gs_scaled))+
-  geom_jitter(aes(col=abs(gs_scaled)>1))+
-  scale_color_manual(values = c("grey","red"))+
-  geom_boxplot(aes(fill=compa),outlier.shape = NA)
 
-p2<-ggplot(resg[compa%in%c("C.L","CF.LF","CM.LM")][abs(gs_scaled)>1][,hyper_meth:=gs_scaled>0])+geom_bar(aes(x=compa,fill=hyper_meth))
-p1+p2
+#OR 
 
+renv::install("bioc::clusterProfiler")
+library(clusterProfiler,)
+library(enrichplot)
+library(org.Hs.eg.db)
+res_kegg<-enrichKEGG(bitr(resg_cl[GeneScore>60&pval<10^-3]$gene,fromType = "SYMBOL",toType = "ENTREZID",OrgDb = org.Hs.eg.db)$ENTREZID,
+                 organism = "hsa",pvalueCutoff = 0.05)
+as.data.frame(res_kegg)
+
+
+emapplot(pairwise_termsim(res_kegg))
+saveRDS(res_kegg,"../methyl/res_kegg_or_cl_temp.rds")
 
 #Integration basal
 source("../methyl/scripts/utils/new_utils.R")
@@ -150,18 +141,15 @@ out<-"../methyl/outputs/figures_paper_meth"
   #Lineage bias
 cbps<-readRDS("../singlecell/outputs/02-hematopo_datasets_integration/cbps0-8_clean/cbps0-8_clean.rds")
 mtd<-data.table(cbps@meta.data,keep.rownames ="bc")
-mtd[,n.sample:=.N,by=c("sample","orig.ident")]
-mtd[,pct.ct:=.N/n.sample,by=c("sample","orig.ident","cell_type")]
-mtd[,pct.lin:=.N/n.sample,by=c("sample","orig.ident","lineage")]
+
 
 cbps_cl<-subset(cbps,hto==F&group%in%c("ctrl","lga")&ambigous==F&orig.ident!="cd34_hto1_0C1I1L")
-mtd_cl<-mtd[hto==F&group%in%c("ctrl","lga")&ambigous==F&orig.ident!="cd34_hto1_0C1I1L"]
-
-
-
-ggplot(unique(mtd_cl[!str_detect(lineage,"unknown")][,.SD,.SDcols=unique(colnames(mtd))],by=c("sample","orig.ident","lineage")))+
+mtd_cl<-data.table(cbps_cl@meta.data,keep.rownames ="bc")
+mtd_cl[,n.sample:=.N,by=c("sample","orig.ident")]
+mtd_cl[,pct.ct:=.N/n.sample,by=c("sample","orig.ident","cell_type")]
+mtd_cl[,pct.lin:=.N/n.sample,by=c("sample","orig.ident","lineage")]
+ggplot(unique(mtd_cl[!str_detect(lineage,"unknown")][,.SD,.SDcols=unique(colnames(mtd_cl))],by=c("sample","orig.ident","lineage")))+
   geom_boxplot(aes(x=group,y=pct.lin,fill=group))+facet_wrap("lineage",scales = "free")
-
 
 
   #DEGs
@@ -231,72 +219,76 @@ res_cl_merge[,correl:=ifelse(gs_scaled>1&avg_logFC>1,"Hypermet-Upreg",
                                ifelse(gs_scaled<(-1)&avg_logFC>1,"Hypomet-Upreg","no change"))))]
 ggplot(res_cl_merge) + geom_point(aes(x=avg_logFC,y=gs_scaled,col=correl))
 
-#kegg
-pathways<-read.gmt("../methyl/ref/msigdb/c2.cp.kegg.v7.1.symbols.gmt")
-pathways_list<-split(pathways$gene,f = pathways$term)
+#enrichKEGG
+library(clusterProfiler)
 
-res_kegg<-over_repr_test_multi(genes_of_interest =res_cl_merge[correl=="Hypermet-Upreg"]$gene ,
-                     terms_list = pathways_list ,
-                     size_universe = nrow(res_cl_merge)
-                    )
+#downreg
+res_kegg<-enrichKEGG(bitr(res_cl_merge[correl=="Hypermet-Downreg"]$gene,fromType = "SYMBOL",toType = "ENTREZID",OrgDb = org.Hs.eg.db)$ENTREZID,
+                 organism = "hsa",pvalueCutoff = 0.1)
 
-res_kegg[padj<0.5]#0
+dotplot(res_kegg,showCategory =20)
 
-#with hypermet-downreg
-res_kegg2<-over_repr_test_multi(genes_of_interest =res_cl_merge[correl=="Hypermet-Downreg"]$gene ,
-                     terms_list = pathways_list ,
-                     size_universe = nrow(res_cl_merge)
-                    )
-
-res_kegg2[padj<0.5]#0
+#upreg
+res_kegg_up<-enrichKEGG(bitr(res_cl_merge[correl=="Hypermet-Upreg"]$gene,fromType = "SYMBOL",toType = "ENTREZID",OrgDb = org.Hs.eg.db)$ENTREZID,
+                 organism = "hsa",pvalueCutoff = 0.1)
+as.data.frame(res_kegg_up) #work
+dotplot(res_kegg_up,showCategory =20)
 
 
-#GO
-go_term<-read.gmt("../methyl/ref/msigdb/c5.go.bp.v7.3.symbols.gmt")
-go_list<-split(go_term$gene,f = go_term$term)
+#INTEGR WITH STRESS
+  #Lineage bias
+cbps_clh<-subset(cbps,hto==T&group%in%c("ctrl","lga")&ambigous==F&orig.ident!="cd34_hto1_0C1I1L")
+mtd_clh<-data.table(cbps_clh@meta.data,keep.rownames ="bc")
+mtd_clh[,n.sample:=.N,by=c("sample","orig.ident")]
+mtd_clh[,pct.ct:=.N/n.sample,by=c("sample","orig.ident","cell_type")]
+mtd_clh[,pct.lin:=.N/n.sample,by=c("sample","orig.ident","lineage")]
+ggplot(unique(mtd_clh[!str_detect(lineage,"unknown")][,.SD,.SDcols=unique(colnames(mtd_clh))],by=c("sample","orig.ident","lineage")))+
+  geom_boxplot(aes(x=group,y=pct.lin,fill=group))+facet_wrap("lineage",scales = "free")
 
-res_go<-over_repr_test_multi(genes_of_interest =res_cl_merge[correl=="Hypermet-Upreg"]$gene ,
-                     terms_list = go_list ,
-                     size_universe = nrow(res_cl_merge)
-                    )
+#correl scdegs with methyl
+res_cl<-fread("../methyl/outputs/model14_without_iugr/2020-09-16_res_C.L_with_GeneScore_and_permut.csv")
+degs_clh_sc<-fread("../singlecell/outputs/07-DEGs_LGA_stress/sc_edger_deseq2_all_cbps/res_de_analysis_all_genes.csv")
+res_clh_merge<-merge(degs_clh_sc,unique(res_cl[order(gene,pval)],by="gene"))
 
-res_go[padj<0.5]#0
-#with hypermet-downreg
-res_go<-over_repr_test_multi(genes_of_interest =res_cl_merge[correl=="Hypermet-Downreg"]$gene ,
-                     terms_list = go_list ,
-                     size_universe = nrow(res_cl_merge)
-                    )
-
-res_go[padj<0.5]#0
-
-
-#GSEA [to do]
-#score correl by gene = distance fold change obs with fold change calc ~ linear regression
-summary(lm(res_cl_merge$avg_logFC~res_cl_merge$GeneScore))
-#a = -0.0005153
-#b=  -0.0162490
-
-PredFoldChange<-function(gene_score,a=-0.0005153,b=-0.0162490)a*gene_score+b
-PredFoldChange(300)
-res_cl_merge[,avg_logFC_pred:=PredFoldChange(GeneScore)]
-ggplot(res_cl_merge)+geom_point(aes(x=GeneScore,y=avg_logFC_pred))
-res_cl_merge[,avg_logFC_dist:=abs(avg_logFC_pred-avg_logFC)]
-res_cl_merge[order(avg_logFC_dist)]
-#doesnt worj because correl based on data
-
-#INTEGR WITH STRRESS
-#validation genescore
-res_de<-rbind(fread("../singlecell/outputs/08-DEGs_LGA_no_stress/pseudobulk_deseq2_all_cbps/res_de_analysis_all_genes.csv")[,condition:="basale"],
-              fread("../singlecell/outputs/07-DEGs_LGA_stress/pseudobulk_deseq2_all_cbps/res_de_analysis_all_genes.csv")[,condition:="stress"])
-
-res_cl_merge<-merge(res_de,res_cl,allow.cartesian=TRUE)
-
-ggplot(res_cl_merge)+geom_boxplot(aes(x=padj<0.1,y=GeneScore))+facet_wrap("condition")          
+ggplot(res_clh_merge) + geom_point(aes(x=-log10(p_val_adj)*avg_logFC,y=GeneScore))
+ggplot(res_clh_merge) + geom_point(aes(x=avg_logFC,y=GeneScore))
 
 
-res_de_sc<-rbind(fread("../singlecell/outputs/08-DEGs_LGA_no_stress/sc_edger_deseq2_all_cbps/res_de_analysis_all_genes.csv")[,condition:="basale"],
-              fread("../singlecell/outputs/07-DEGs_LGA_stress/sc_edger_deseq2_all_cbps/res_de_analysis_all_genes.csv")[,condition:="stress"])
+#enrichKEGG with clusterprofiler
 
-res_cl_sc_merge<-merge(res_de_sc,res_cl,allow.cartesian=TRUE)
+res_keggh<-enrichKEGG(bitr(res_clh_merge[correl=="Hypermet-Downreg"]$gene,fromType = "SYMBOL",toType = "ENTREZID",OrgDb = org.Hs.eg.db)$ENTREZID,
+                 organism = "hsa",pvalueCutoff = 0.1)
+as.data.frame(res_keggh) #work
+dotplot(res_keggh,showCategory =20)
 
-ggplot(res_cl_sc_merge)+geom_boxplot(aes(x=p_val_adj<0.1,y=GeneScore))+facet_wrap("condition")          
+emapplot(pairwise_termsim(res_keggh))
+
+res_keggh_up<-enrichKEGG(bitr(res_clh_merge[correl=="Hypermet-Upreg"]$gene,fromType = "SYMBOL",toType = "ENTREZID",OrgDb = org.Hs.eg.db)$ENTREZID,
+                 organism = "hsa",pvalueCutoff = 0.1)
+as.data.frame(res_keggh_up) #work
+dotplot(res_keggh_up,showCategory =20)
+
+#wnt and growth hormone genes
+genes_wnt<-tr(as.data.frame(res_keggh)["hsa04310","geneID"],tradEntrezInSymbol = T)
+genes_growth<-tr(as.data.frame(res_keggh)["hsa04935","geneID"],tradEntrezInSymbol = T)
+
+res_keggh_dt<-data.table(as.data.frame(res_keggh))
+res_keggh_dt[,gene:=paste(tr(geneID,tradEntrezInSymbol = T),collapse = "|"),by="ID"]
+res_keggh_dt[str_detect(gene,"HES1")]
+genes_of_interest<-union(genes_wnt,genes_growth)
+#HSC specific
+DefaultAssay(cbps_clh)<-"SCT"
+Idents(cbps_clh)<-"lineage"
+FeaturePlot(cbps_clh,features =genes_of_interest[1:6] , max.cutoff = "q95")
+FeaturePlot(cbps_clh,features =genes_of_interest[7:length(genes_of_interest)], max.cutoff = "q95")
+FeaturePlot(cbps_clh,features ="SOCS3",label=T ,max.cutoff = "q95")
+FeaturePlot(cbps_clh,features ="SOCS1",label=T ,max.cutoff = "q95")
+FeaturePlot(cbps_clh,features ="JUN",label=T )
+FeaturePlot(cbps_clh,features ="FOS",label=T )
+FeaturePlot(cbps_clh,features ="HES1",label=T ,max.cutoff = "q95")
+
+#run
+degs_clh_ps_lin<-fread("../singlecell/outputs/07-DEGs_LGA_stress/pseudobulk_deseq2_by_lin/res_de_analysis_all_genes.csv")
+table(degs_clh_ps_lin[padj<0.05]$lineage)
+
+ggplot(degs_clh_ps_lin[padj<0.05])+geom_bar(aes(x=lineage,fill=lineage))
