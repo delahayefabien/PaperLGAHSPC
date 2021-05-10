073a57a23d69eb4a18ed5fdbf13a541d3c57c13c
Alexandre Pelletier <apelletier@egid.local>
2021-05-10 10:14:12 +0200

    gwas analysis

--- scripts/03-pathway_analysis.R
+++ scripts/03-pathway_analysis.R
@@ -72,3 +72,57 @@ fwrite(gsea_go[order(p.adjust)],fp(out,"res_gsea_go.csv"))
 dotplot(res_gsea_go,x=gsea_go[order(p.adjust)]$gene_score.avg[1:40],showCategory=40)
 
 emapplot(pairwise_termsim(res_gsea_go,showCategory = 40),showCategory = 40)
+
+#GSE GWAS
+source("scripts/utils/new_utils.R")
+library(clusterProfiler)
+library(enrichplot)
+resg<-fread("outputs/02-genescore_calculation_and_validation/res_genes.csv.gz")
+gwas_genes<-fread("ref/gwas/list_ref_GWAS_042721.csv",
+                  select = c(2,3,4),
+                  col.names = c("reported_gene","mapped_genes","disease_trait"),
+                  skip = 1)
+
+#☺1) with reported genes
+gwas_genes[reported_gene=="NR",reported_gene:=NA]
+gwas_genes_rep<-unique(gwas_genes[!is.na(reported_gene)],by=c("reported_gene","disease_trait"))
+gwas_genes_rep #92k gene-trait associations
+length(unique(gwas_genes_rep$disease_trait)) #4k5 disease traits
+gwas_genes_rep[,n.gene.trait:=.N,by="disease_trait"]
+summary(unique(gwas_genes_rep[,.(disease_trait,n.gene.trait)])$n.gene.trait)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 1.00    2.00    5.00   20.38   11.00 1532.00 
+
+# remove traits if < 10 genes 
+gwas_genes_rep10<-gwas_genes_rep[n.gene.trait>=10]
+gwas_genes_rep10 #81k gene-trait associations
+length(unique(gwas_genes_rep10$disease_trait)) #1299 disease traits
+gwas_genes_rep10[,n.gene.trait:=.N,by="disease_trait"]
+summary(unique(gwas_genes_rep10[,.(disease_trait,n.gene.trait)])$n.gene.trait)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 10.00   14.00   23.00   62.75   50.00 1532.00 
+
+gene_scores<-resg$gene_score_add
+names(gene_scores)<-resg$gene
+gene_scores<-sort(gene_scores,decreasing = T)
+
+# GSEA
+?GSEA
+res_gsea_gwas<-GSEA(geneList = rank(gene_scores),
+                    TERM2GENE = gwas_genes_rep10[,.(disease_trait,reported_gene)],
+                    maxGSSize    = 500,
+                    eps = 0,
+                    pvalueCutoff = 0.01
+                    )
+nrow(as.data.frame(res_gsea_gwas))#83/1300
+dotplot(res_gsea_gwas,showCategory=20)
+emapplot(pairwise_termsim(res_gsea_gwas,showCategory = 83),showCategory = 83)
+
+gsea_gwas<-data.table(as.data.frame(res_gsea_gwas))
+gsea_gwas[,gene_score.avg:=mean(resg$gene_score_add[resg$gene %in% tr(core_enrichment,tradEntrezInSymbol = F)],na.rm=T),.(ID)]
+
+saveRDS(res_gsea_gwas,fp(out,"res_gsea_gwas.rds"))
+fwrite(gsea_gwas[order(p.adjust)],fp(out,"res_gsea_go.csv"))
+
+
+#2) if not satisfying, with mapped gene #satisfying
